ee34a7a2ef4ec55994dc02a120620e1bade22f6f
ebalo <e.balsamo000@gmail.com>
2021-04-27 17:38:32 +0200

    install command, crud helper, crud trait

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -11,7 +11,7 @@ class Install extends Command
      *
      * @var string
      */
-    protected $signature = 'easyCrud:install';
+    protected $signature = 'easy-crud:install';
 
     /**
      * The console command description.
